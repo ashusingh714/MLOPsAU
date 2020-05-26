db1acd0a6bf61d40e06a1329d78f7830ac2d7cfb
Ashutosh Kumar Singh <singhashutoshkumar889@gmail.com>
2020-05-26 18:59:03 +0530

    third

--- Mnist.py
+++ Mnist.py
@@ -43,8 +43,7 @@ model.summary()
 test_loss, test_acc = model.evaluate(test_X, test_Y_one_hot)
 print('Test loss', test_loss)
 print('Test accuracy', test_acc)
-predictions = model.predict(test_X)
-print(np.argmax(np.round(predictions[i])))
+
 
 
 
